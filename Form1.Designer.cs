0e823cadec7587ee76025ee2cde425ee5478b552
Alexander <alekogureshidze@gmail.com>
2023-05-18 14:28:15 +0400

    Initial Import

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WasapiLoopback
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRecord = new System.Windows.Forms.Button();
+            this.btnFinish = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRecord
+            // 
+            this.btnRecord.Location = new System.Drawing.Point(76, 141);
+            this.btnRecord.Name = "btnRecord";
+            this.btnRecord.Size = new System.Drawing.Size(148, 57);
+            this.btnRecord.TabIndex = 0;
+            this.btnRecord.Text = "Record";
+            this.btnRecord.UseVisualStyleBackColor = true;
+            this.btnRecord.Click += new System.EventHandler(this.btnRecord_Click);
+            // 
+            // btnFinish
+            // 
+            this.btnFinish.Location = new System.Drawing.Point(320, 141);
+            this.btnFinish.Name = "btnFinish";
+            this.btnFinish.Size = new System.Drawing.Size(148, 57);
+            this.btnFinish.TabIndex = 1;
+            this.btnFinish.Text = "Finish";
+            this.btnFinish.UseVisualStyleBackColor = true;
+            this.btnFinish.Click += new System.EventHandler(this.btnFinish_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFinish);
+            this.Controls.Add(this.btnRecord);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRecord;
+        private System.Windows.Forms.Button btnFinish;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+
+namespace WasapiLoopback
+{
+    public partial class Form1 : Form
+    {
+        private string strOut = "out.wav";
+        private string strSource = "source.wav";
+        public Form1()
+        {
+            InitializeComponent();
+            if (File.Exists(strSource))
+                File.Delete(strSource);
+            if (File.Exists(strOut))
+                File.Delete(strOut);
+        }
+
+        private void btnRecord_Click(object sender, EventArgs e)
+        {
+            captureDevice = CreateWaveInDevice();
+
+            writer = new WaveFileWriter(strOut, captureDevice.WaveFormat);
+            captureDevice.StartRecording();
+        }
+
+        private IWaveIn CreateWaveInDevice()
+        {
+            Console.WriteLine($"-------------------{Thread.CurrentThread.ManagedThreadId}");
+            var deviceEnum = new MMDeviceEnumerator();
+            var renderDevices = deviceEnum.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active).ToList();
+            renderDevices.ForEach(d=>Console.WriteLine(d.FriendlyName));
+
+            var device = (MMDevice)renderDevices[1];
+            IWaveIn newWaveIn = new WasapiCapture(device);
+            // Both WASAPI and WaveIn support Sample Rate conversion!
+            var sampleRate = (int)16000;
+            var channels = 0 + 1;
+            newWaveIn.WaveFormat = new WaveFormat(sampleRate, channels);
+
+            newWaveIn.DataAvailable += OnDataAvailable;
+            newWaveIn.RecordingStopped += OnRecordingStopped;
+            return newWaveIn;
+        }
+
+        private void OnRecordingStopped(object sender, StoppedEventArgs e)
+        {
+            Console.WriteLine("OnRecordingStopped");
+            writer?.Dispose();
+            writer = null;
+        }
+
+        private WaveFileWriter writer;
+        private void OnDataAvailable(object sender, WaveInEventArgs e)
+        {
+            //Console.WriteLine($"OnDataAvailable: {e.BytesRecorded} {Thread.CurrentThread.ManagedThreadId}");
+
+#if true
+            //Append binary data to file
+            //Test to write callback data directly into file
+            using (var fileStream = new FileStream(strSource, FileMode.Append,
+                       FileAccess.Write, FileShare.None))
+            using (var bw = new BinaryWriter(fileStream))
+            {
+                bw.Write(e.Buffer, 0, e.BytesRecorded);
+            }
+#endif
+
+
+            writer.Write(e.Buffer, 0, e.BytesRecorded);
+        }
+
+        private IWaveIn captureDevice;
+        private void btnFinish_Click(object sender, EventArgs e)
+        {
+            captureDevice?.StopRecording();
+        }
+    }
+}
